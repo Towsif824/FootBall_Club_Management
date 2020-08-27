504d73502f7b8bba586785267bd8bcd73d89341a
Towsif824 <tawsif824@gmail.com>
2020-08-27 18:10:55 +0600

    football_club

---
+++ Final_Football_Club/Final_Football_Club/Controllers/PNotificationController.cs
@@ -0,0 +1,20 @@
+﻿using Final_Football_Club.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Football_Club.Controllers
+{
+    public class PNotificationController : Controller
+    {
+        ClubEntities2 noti = new ClubEntities2();
+        // GET: PNotification
+        public ActionResult Index()
+        {
+            var data = Session["UserName"];
+            return View(noti.PNotifications.Where(x => x.UserName == data).OrderByDescending(x => x.Date).ToList());
+        }
+    }
+}
